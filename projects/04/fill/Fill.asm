240f89c531e2fb2078c18afc0c4cebcdcea7a3f2
Vincent Perez <perezv815@gmail.com>
2020-03-04 12:04:39 +0100

    project 04: Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,62 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// while True
+(LOOP)
+// pixeladdress=16384
+@SCREEN
+D=A
+@pixeladdress
+M=D
+// while True
+(WHILEBLACK)
+// if no key pressed break
+@24576
+D=M
+@ENDWHILEBLACK
+D;JEQ
+// blacken pixel at pixeladdress
+@pixeladdress
+A=M
+M=-1
+// pixeladdress++
+@pixeladdress
+M=M+1
+D=M
+// if pixeladdress < 24576 goto start of WHILEBLACK
+@24576
+D=A-D
+@WHILEBLACK
+D;JGE
+// goto main loop
+@LOOP
+0;JMP
+(ENDWHILEBLACK)
+// pixeladdress=16384
+@SCREEN
+D=A
+@pixeladdress
+M=D
+// while True
+(WHILEWHITE)
+// if some key is pressed break
+@24576
+D=M
+@LOOP
+D;JNE
+// whiten pixel at pixeladdress
+@pixeladdress
+A=M
+M=0
+// pixeladdress++
+@pixeladdress
+M=M+1
+D=M
+// if pixeladdress < 24576 goto start of WHILEBLACK
+@24576
+D=A-D
+@WHILEWHITE
+D;JGE
+// goto main loop
+@LOOP
+0;JMP
