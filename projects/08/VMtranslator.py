df212d5a9b2f032244767b2f86e59487c237dd47
Vincent Perez <perezv815@gmail.com>
2020-03-09 16:12:41 +0100

    project 08: ProgramFLow tests are working

--- projects/08/VMtranslator.py
+++ projects/08/VMtranslator.py
@@ -29,14 +29,21 @@ class Parser:
             return "C_PUSH"
         if current_cmd.startswith("pop"):
             return "C_POP"
+        if current_cmd.startswith("label"):
+            return "C_LABEL"
+        if current_cmd.startswith("goto"):
+            return "C_GOTO"
+        if current_cmd.startswith("if-goto"):
+            return "C_IF"
         return "C_ARITHMETIC"
 
     def arg1(self):
         cmd_type = self.command_type()
+        if cmd_type == "C_ARITHMETIC":
+            return self.current_command
+
         args = self.current_command.split()
-        if cmd_type in ["C_PUSH", "C_POP"]:
-            return args[1]
-        return self.current_command
+        return args[1]
 
     def arg2(self):
         cmd_type = self.command_type()
@@ -57,34 +64,34 @@ class CodeWriter:
         self._stream = stream
         self._filename = None
         self._asm_cmd_index = 0
+        self._current_function = "Sys.init"
 
     def set_filename(self, filename):
         self._filename = filename
 
     def write_arithmetic(self, cmd):
         if cmd == "add":
-            self._write_binary_op(cmd, "+")
+            self._write_binary_op("+")
         elif cmd == "sub":
-            self._write_binary_op(cmd, "-")
+            self._write_binary_op("-")
         elif cmd == "and":
-            self._write_binary_op(cmd, "&")
+            self._write_binary_op("&")
         elif cmd == "or":
-            self._write_binary_op(cmd, "|")
+            self._write_binary_op("|")
         elif cmd == "neg":
-            self._write_unary_op(cmd, "-")
+            self._write_unary_op("-")
         elif cmd == "not":
-            self._write_unary_op(cmd, "!")
+            self._write_unary_op("!")
         elif cmd == "eq":
-            self._write_compare_op(cmd, "JEQ")
+            self._write_compare_op("JEQ")
         elif cmd == "lt":
-            self._write_compare_op(cmd, "JLT")
+            self._write_compare_op("JLT")
         elif cmd == "gt":
-            self._write_compare_op(cmd, "JGT")
+            self._write_compare_op("JGT")
         else:
             raise ValueError(f"unrecognized arithmetic operation: {cmd}")
 
-    def _write_binary_op(self, cmd, op):
-        self._write_asm_cmd(f"// {cmd}")
+    def _write_binary_op(self, op):
         self._write_asm_cmd("@SP")
         self._write_asm_cmd("A=M-1")
         self._write_asm_cmd("D=M")
@@ -93,14 +100,12 @@ class CodeWriter:
         self._write_asm_cmd("@SP")
         self._write_asm_cmd("M=M-1")
 
-    def _write_unary_op(self, cmd, op):
-        self._write_asm_cmd(f"// {cmd}")
+    def _write_unary_op(self, op):
         self._write_asm_cmd("@SP")
         self._write_asm_cmd("A=M-1")
         self._write_asm_cmd(f"M={op}M")
 
-    def _write_compare_op(self, cmd, jmp):
-        self._write_asm_cmd(f"// {cmd}")
+    def _write_compare_op(self, jmp):
         self._write_asm_cmd("@SP")
         self._write_asm_cmd("A=M-1")
         self._write_asm_cmd("D=M")
@@ -123,9 +128,9 @@ class CodeWriter:
 
     def write_push_pop(self, cmd, segment, index):
         if cmd == "C_PUSH":
-            self._write_push(cmd, segment, index)
+            self._write_push(segment, index)
         elif cmd == "C_POP":
-            self._write_pop(cmd, segment, index)
+            self._write_pop(segment, index)
 
     def _get_segment_base(self, segment, index):
         if segment == "local":
@@ -144,9 +149,8 @@ class CodeWriter:
             return f"{self._filename}.{index}"
         raise ValueError(f"unknown segment {segment}")
 
-    def _write_push(self, cmd, segment, index):
+    def _write_push(self, segment, index):
         if segment == "constant":
-            self._write_asm_cmd(f"// {cmd} {segment} {index}")
             self._write_asm_cmd(f"@{index}")
             self._write_asm_cmd("D=A")
             self._write_asm_cmd("@SP")
@@ -157,7 +161,6 @@ class CodeWriter:
             return
 
         base = self._get_segment_base(segment, index)
-        self._write_asm_cmd(f"// {cmd} {segment} {index}")
         # put in D the content of the address we want to push from
         self._write_asm_cmd(f"@{base}")
         if segment in ["pointer", "temp"]:
@@ -176,12 +179,11 @@ class CodeWriter:
         self._write_asm_cmd("@SP")
         self._write_asm_cmd("M=M+1")
 
-    def _write_pop(self, cmd, segment, index):
+    def _write_pop(self, segment, index):
         if segment == "constant":
             raise ValueError("cannot pop to constant segment")
 
         base = self._get_segment_base(segment, index)
-        self._write_asm_cmd(f"// {cmd} {segment} {index}")
         # put in D the address we want to pop to
         self._write_asm_cmd(f"@{base}")
         if segment in ["pointer", "temp", "static"]:
@@ -206,11 +208,26 @@ class CodeWriter:
         self._write_asm_cmd("@SP")
         self._write_asm_cmd("M=M-1")
 
+    def write_label(self, label):
+        self._write_asm_cmd(f"({self._build_asm_label(label)})")
+
+    def write_goto(self, label):
+        self._write_asm_cmd(f"@{self._build_asm_label(label)}")
+        self._write_asm_cmd("0;JMP")
+
+    def write_if(self, label):
+        self._write_asm_cmd("@SP")
+        self._write_asm_cmd("M=M-1")
+        self._write_asm_cmd("A=M")
+        self._write_asm_cmd("D=M")
+        self._write_asm_cmd(f"@{self._build_asm_label(label)}")
+        self._write_asm_cmd("D;JNE")
+
     def _write_asm_cmd(self, cmd):
         self._stream.write(f"{cmd}\n")
-        # do not count comments
-        if not cmd.startswith("//"):
-            self._asm_cmd_index += 1
+
+    def _build_asm_label(self, label):
+        return f"{self._current_function}${label}"
 
     def close(self):
         self._stream.close()
@@ -235,5 +252,14 @@ if __name__ == "__main__":
             segment = parser.arg1()
             index = parser.arg2()
             code_writer.write_push_pop(cmd_type, segment, index)
+        elif cmd_type == "C_LABEL":
+            label = parser.arg1()
+            code_writer.write_label(label)
+        elif cmd_type == "C_GOTO":
+            label = parser.arg1()
+            code_writer.write_goto(label)
+        elif cmd_type == "C_IF":
+            label = parser.arg1()
+            code_writer.write_if(label)
 
     code_writer.close()
